d7b0cdad4ac3373d5a9e6be0c73291d7069d6e84
woorunwoo <jiyong1972@gmail.com>
2021-10-16 03:03:02 +0900

    Fuego Web Page

---
+++ fuegoweb/myblog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Posts
+
+
+@admin.register(Posts)
+class PostsAdmin(admin.ModelAdmin):
+    pass
---
+++ fuegoweb/myblog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Posts(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=1000)
+    image_path = models.CharField(max_length=255, unique=True)
+    updated_time = models.DateTimeField(default=now)
+    user_id = models.IntegerField()
