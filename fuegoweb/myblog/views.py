e9508a128cd85b8c0d2e0a8e050466afb30e8471
woorunwoo <jiyong1972@gmail.com>
2021-10-17 20:03:11 +0900

    add flake8 to requirements.txt

--- fuegoweb/myblog/views.py
+++ fuegoweb/myblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from myblog.serializers import PostsSerializer
